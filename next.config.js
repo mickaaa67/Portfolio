ee2f42f4ee7de217385438dd893b433546f121af
Micka67 <mickael.hoffer@gmail.com>
2025-10-10 02:19:49 +0200

    ajout appdir pour src/app

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true, // Important pour Next.js 13+ avec src/app
+  },
+};
+
+module.exports = nextConfig;
