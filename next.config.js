f958bb6e08ecd6ce387bc9e466906616b0d2c9bc
Micka67 <mickael.hoffer@gmail.com>
2025-10-11 17:14:50 +0200

    aa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
+  experimental: {
+    appDir: true,
+  },
+  // Indique à Next.js que ton app est dans src/
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
